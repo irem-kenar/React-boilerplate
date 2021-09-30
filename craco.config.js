892226cb3957dfe164f9ab1c0d528bee8401fa58
Irem Kenar <ikenar@kazandirio-dev.com>
2021-09-30 11:01:33 +0300

    Added craco

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+const {
+    when,
+    whenDev,
+    whenProd,
+    whenTest,
+    ESLINT_MODES,
+    POSTCSS_MODES,
+} = require("@craco/craco");
+
+module.exports = {
+    eslint: {
+        mode: "file",
+    },
+};
